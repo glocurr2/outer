ca6cfe579e40f02a81f7b96982a038116c0543f5
unknown <gloriacurran@graymatters-inc.com>
2025-05-30 15:57:01 -0400

    change image links

--- src/components/BookHeader.vue
+++ src/components/BookHeader.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="center-header">
     <div>
-      <img class="book-rocket" src="./images/book-rocket.png" alt="outerspace book art" />
+      <img
+        class="book-rocket"
+        src="./src/assets/images/book-rocket.png"
+        alt="outerspace book art"
+      />
     </div>
     <div class="logo-tagline">
-      <img class="logo" src="./images/logo.png" alt="outerspace book logo" />
+      <img class="logo" src="./src/assets/images/logo.png" alt="outerspace book logo" />
       <div class="tagline">
         A Guide to the Top 100 Science Fiction<br />and Fantasy Books of all Time
       </div>
